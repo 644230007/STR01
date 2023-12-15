518cd031419f3f18cea79e2a5b55336df22f1d90
Nice <644230007@webmail.npru.ac.th>
2023-12-15 13:25:17 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -4,3 +4,11 @@ st.title('การทดสอบเขียนเว็บด้วย Python
 st.header("Chuthiwat khamsakhon")
 st.subheader('สาขาวิชาเทคโนโลยีสารสนเทศ')
 st.markdown("----")
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/nice.jpg')
+with col2:
+    st.image('./pic/iris.jpg')
