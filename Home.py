b6ae48288aed99d1e30b4ae2d8a2a1a348f9a1a6
Nice <644230007@webmail.npru.ac.th>
2023-12-15 14:46:33 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,12 +1,4 @@
 import streamlit as st
-import json
-from streamlit_lottie import st_lottie
-
-def load_lottieurl(url: str):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
 
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header("Chuthiwat khamsakhon")
