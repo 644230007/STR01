713153205beb198d64fee20fd6f9755c6842d4ed
Nice <644230007@webmail.npru.ac.th>
2023-12-15 13:32:52 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -12,3 +12,11 @@ with col1:
     st.image('./pic/nice.jpg')
 with col2:
     st.image('./pic/iris.jpg')
+
+html_1 = """
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
+"""
+st.markdown(html_1, unsafe_allow_html=True)
+st.markdown("")
