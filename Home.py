7a0bbf57f40be766c2fd04714d8541ad9b142ff4
Nice <644230007@webmail.npru.ac.th>
2023-12-15 13:49:50 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -47,3 +47,10 @@ html_2 = """
 """
 st.markdown(html_2, unsafe_allow_html=True)
 st.markdown("")
+
+
+ptlen = st.slider("กรุณาเลือกข้อมูล petal.length",0,10)
+ptwd = st.slider("กรุณาเลือกข้อมูล petal.width",0,10)
+
+splen = st.number_input("กรุณาเลือกข้อมูล sepal.length")
+spwd = st.number_input("กรุณาเลือกข้อมูล sepal.width")
